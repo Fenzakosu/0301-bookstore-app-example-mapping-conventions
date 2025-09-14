3a99770147252c8c0f9fd04b8a50dca244415568
Fenzakosu <lukakovacevic266@gmail.com>
2025-09-14 11:40:57 +0200

    Kreirana v2 migracija

--- BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
+++ BookstoreApplication/BookstoreApplication/Models/AppDbContext.cs
@@ -20,17 +20,40 @@ namespace BookstoreApplication.Models
         {
             
             modelBuilder.Entity<AuthorAward>()
+                .ToTable("AuthorAwardBridge") 
                 .HasKey(authorAward => new { authorAward.AuthorId, authorAward.AwardId });
 
+            
             modelBuilder.Entity<AuthorAward>()
                 .HasOne(authorAward => authorAward.Author)
                 .WithMany(authorAward => authorAward.AuthorAwards)
-                .HasForeignKey(authorAward => authorAward.AuthorId);
+                .HasForeignKey(authorAward => authorAward.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
 
+            
             modelBuilder.Entity<AuthorAward>()
                 .HasOne(authorAward => authorAward.Award)
                 .WithMany(authorAward => authorAward.AuthorAwards)
-                .HasForeignKey(authorAward => authorAward.AwardId);
+                .HasForeignKey(authorAward => authorAward.AwardId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            
+            modelBuilder.Entity<Author>()
+                .Property(author => author.DateOfBirth)
+                .HasColumnName("Birthday");
+
+            
+            modelBuilder.Entity<Book>()
+                .HasOne(book => book.Publisher)
+                .WithMany()
+                .HasForeignKey(book => book.PublisherId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            
+            modelBuilder.Entity<Book>()
+                .HasOne(book => book.Author)
+                .WithMany()
+                .HasForeignKey(book => book.AuthorId);
 
             base.OnModelCreating(modelBuilder);
         }
